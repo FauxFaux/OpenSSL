ef605e8a9018cf56b58a766f95e135734f38a854
OpenSSL Project <openssl-cvs@openssl.org>
2006-05-04 14:21:31 +0100

    openssl-0.9.8b.tar.gz 99565db630a044fa484d4f91006a31908f262246
    $ gpg -v openssl-0.9.8b.tar.gz.asc
    gpg: armor header: Version: GnuPG v1.4.2.2 (GNU/Linux)
    gpg: assuming signed data in `../openssl-0.9.8b.tar.gz'
    gpg: Signature made Thu 04 May 2006 14:23:49 BST using RSA key ID F295C759
    gpg: using PGP trust model
    gpg: Good signature from "Dr Stephen Henson <shenson@drh-consultancy.co.uk>"
    gpg:                 aka "Dr S N Henson <shenson@drh-consultancy.demon.co.uk>"
    gpg:                 aka "Dr Stephen Henson <stephen.henson@opennetworksecurity.com>"
    gpg: WARNING: This key is not certified with sufficiently trusted signatures!
    gpg:          It is not certain that the signature belongs to the owner.
    Primary key fingerprint: D0 5D 8C 61 6E 27 E6 60  41 EC B1 B8 D5 7E E5 97
    gpg: binary signature, digest algorithm SHA1

--- ssl/kssl.c
+++ ssl/kssl.c
@@ -70,7 +70,7 @@
 
 #define _XOPEN_SOURCE 500 /* glibc2 needs this to declare strptime() */
 #include <time.h>
-#if 0 /* Experimental */
+#if 0 /* experimental */
 #undef _XOPEN_SOURCE /* To avoid clashes with anything else... */
 #endif
 #include <string.h>
@@ -1132,7 +1132,7 @@ kssl_cget_tkt(	/* UPDATE */	KSSL_CTX *kssl_ctx,
 	if (authenp)
                 {
 		krb5_data	krb5in_data;
-		unsigned char	*p;
+		const unsigned char	*p;
 		long		arlen;
 		KRB5_APREQBODY	*ap_req;
 
@@ -1301,7 +1301,7 @@ kssl_sget_tkt(	/* UPDATE */	KSSL_CTX		*kssl_ctx,
 	static krb5_auth_context	krb5auth_context = NULL;
 	krb5_ticket 			*krb5ticket = NULL;
 	KRB5_TKTBODY 			*asn1ticket = NULL;
-	unsigned char			*p;
+	const unsigned char		*p;
 	krb5_keytab 			krb5keytab = NULL;
 	krb5_keytab_entry		kt_entry;
 	krb5_principal			krb5server;
@@ -1986,7 +1986,8 @@ krb5_error_code  kssl_check_authent(
 	EVP_CIPHER_CTX		ciph_ctx;
 	const EVP_CIPHER	*enc = NULL;
 	unsigned char		iv[EVP_MAX_IV_LENGTH];
-	unsigned char		*p, *unenc_authent;
+	const unsigned char	*p;
+	unsigned char		*unenc_authent;
 	int 			outl, unencbufsize;
 	struct tm		tm_time, *tm_l, *tm_g;
 	time_t			now, tl, tg, tr, tz_offset;
