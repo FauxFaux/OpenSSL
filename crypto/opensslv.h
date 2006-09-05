5c18cdc57b204b4f559d99f5ef6ea5f2b85efcd2
OpenSSL Project <openssl-cvs@openssl.org>
2006-09-05 10:04:33 +0100

    openssl-0.9.8c.tar.gz d0798e5c7c4509d96224136198fa44f7f90e001d
    $ gpg -v openssl-0.9.8c.tar.gz.asc
    gpg: armor header: Version: GnuPG v1.4.2.2 (GNU/Linux)
    gpg: assuming signed data in `../openssl-0.9.8c.tar.gz'
    gpg: Signature made Tue 05 Sep 2006 09:54:00 BST using RSA key ID 49A563D9
    gpg: using PGP trust model
    gpg: Good signature from "Mark Cox <mjc@redhat.com>"
    gpg:                 aka "Mark Cox <mark@awe.com>"
    gpg:                 aka "Mark Cox <mjc@apache.org>"
    gpg: WARNING: This key is not certified with sufficiently trusted signatures!
    gpg:          It is not certain that the signature belongs to the owner.
    Primary key fingerprint: 7B 79 19 FA 71 6B 87 25  0E 77 21 E5 52 D9 83 BF
    gpg: binary signature, digest algorithm SHA1

--- crypto/opensslv.h
+++ crypto/opensslv.h
@@ -12,7 +12,7 @@
  * 0.9.3-beta2    0x00903002 (same as ...beta2-dev)
  * 0.9.3	  0x0090300f
  * 0.9.3a	  0x0090301f
- * 0.9.4 	  0x0090400f
+ * 0.9.4	  0x0090400f
  * 1.2.3z	  0x102031af
  *
  * For continuity reasons (because 0.9.5 is already out, and is coded
@@ -25,11 +25,11 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x009070bfL
+#define OPENSSL_VERSION_NUMBER	0x0090803f
 #ifdef OPENSSL_FIPS
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7k-fips 05 Sep 2006"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8c-fips 05 Sep 2006"
 #else
-#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.7k 05 Sep 2006"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 0.9.8c 05 Sep 2006"
 #endif
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
@@ -83,7 +83,7 @@
  * should only keep the versions that are binary compatible with the current.
  */
 #define SHLIB_VERSION_HISTORY ""
-#define SHLIB_VERSION_NUMBER "0.9.7"
+#define SHLIB_VERSION_NUMBER "0.9.8"
 
 
 #endif /* HEADER_OPENSSLV_H */
